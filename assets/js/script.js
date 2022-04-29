109b0326bce174a4ad25d052c7e38846b51e6a58
jcode5577 <99107295+jcode5577@users.noreply.github.com>
2022-04-29 11:40:00 -0500

    made boxes larger, added dropbox feature/javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,30 @@ const navbarMenu = document.querySelector('#nav-links');
 selectIcon.addEventListener('click', () => {
   navbarMenu.classList.toggle('is-active');
 });
+
+// Get all dropdowns on the page that aren't hoverable.
+const dropdowns = document.querySelectorAll('.dropdown:not(.is-hoverable)');
+
+if (dropdowns.length > 0) {
+  // For each dropdown, add event handler to open on click.
+  dropdowns.forEach(function(el) {
+    el.addEventListener('click', function(e) {
+      e.stopPropagation();
+      el.classList.toggle('is-active');
+    });
+  });
+
+  // If user clicks outside dropdown, close it.
+  document.addEventListener('click', function(e) {
+    closeDropdowns();
+  });
+}
+
+/*
+ * Close dropdowns by removing `is-active` class.
+ */
+function closeDropdowns() {
+  dropdowns.forEach(function(el) {
+    el.classList.remove('is-active');
+  });
+}
