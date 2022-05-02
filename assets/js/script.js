62ede90207d3dafc76ece8f50e4e876797df0dde
jcode5577 <99107295+jcode5577@users.noreply.github.com>
2022-05-02 10:25:20 -0500

    added more food catagories/dietary restrictions/added footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,35 @@
-
 // mobile menu
-const selectIcon = document.querySelector('#select');
-const navbarMenu = document.querySelector('#nav-links');
+const selectIcon = document.querySelector("#select");
+const navbarMenu = document.querySelector("#nav-links");
 const dropdownItems = document.querySelectorAll(".dropdown-item");
 //toggles dropdown box input. example "italian" or "mexican"
-dropdownItems.forEach(el => {
+dropdownItems.forEach((el) => {
   el.addEventListener("click", (event) => {
     const targetId = event.target.getAttribute("data-target");
 
-    const targetElement = document.querySelector("#"+targetId);
+    const targetElement = document.querySelector("#" + targetId);
     targetElement.textContent = event.target.textContent;
-  })
-})
+  });
+});
 
-selectIcon.addEventListener('click', () => {
-  navbarMenu.classList.toggle('is-active');
+selectIcon.addEventListener("click", () => {
+  navbarMenu.classList.toggle("is-active");
 });
 
 // Get all dropdowns on the page that aren't hoverable.
-const dropdowns = document.querySelectorAll('.dropdown:not(.is-hoverable)');
+const dropdowns = document.querySelectorAll(".dropdown:not(.is-hoverable)");
 
 if (dropdowns.length > 0) {
   // For each dropdown, add event handler to open on click.
-  dropdowns.forEach(function(el) {
-    el.addEventListener('click', function(e) {
+  dropdowns.forEach(function (el) {
+    el.addEventListener("click", function (e) {
       e.stopPropagation();
-      el.classList.toggle('is-active');
+      el.classList.toggle("is-active");
     });
   });
 
   // If user clicks outside dropdown, close it.
-  document.addEventListener('click', function(e) {
+  document.addEventListener("click", function (e) {
     closeDropdowns();
   });
 }
@@ -39,7 +38,7 @@ if (dropdowns.length > 0) {
  * Close dropdowns by removing `is-active` class.
  */
 function closeDropdowns() {
-  dropdowns.forEach(function(el) {
-    el.classList.remove('is-active');
+  dropdowns.forEach(function (el) {
+    el.classList.remove("is-active");
   });
 }
