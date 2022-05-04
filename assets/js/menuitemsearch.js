cd2e85630a4e6f065c35b131f94959994f39bc18
KevinJ3259 <kmjordan2009.kj@gmail.com>
2022-05-04 15:24:24 -0500

    completed menu item search

--- assets/js/menuitemsearch.js
+++ assets/js/menuitemsearch.js
@@ -1,13 +1,36 @@
-const apiKey="3dda98fa1ab845c5b8da209e969f31ae";
-const form=document.getElementById("menu-item-search");
-form.addEventListener("submit",async function(event){
-   event.preventDefault();
-   
-  const formData=new FormData(event.target);
-  const itemName=formData.get("item-name");
-  alert("Hi");
-const apiResponse=await fetch(`https://api.spoonacular.com/food/menuItems/search?apiKey=${apiKey}&query=${itemName}`);
-const apiBody=await apiResponse.json()
+(function() {
+const form = document.getElementById("menu-item-search");
+const restaurantsEl = document.getElementById("restaurants");
 
-console.log(apiBody.menuItems)
-})
+form.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const itemName = formData.get("item-name");
+
+  const apiResponse = await fetch(`https://api.spoonacular.com/food/menuItems/search?apiKey=${apiKey}&query=${itemName}`);
+  const apiBody = await apiResponse.json();
+
+  console.log(apiBody.menuItems);
+  restaurantsEl.innerHTML = "";
+  apiBody.menuItems.forEach(createMenuItem);
+});
+
+function createMenuItem(menuItem) {
+  const menuItemEl = document.createElement("div");
+
+  const menuItemNameEl = document.createElement("div");
+  menuItemNameEl.textContent = menuItem.title;
+  menuItemEl.appendChild(menuItemNameEl);
+
+  const menuItemRestaurantEl = document.createElement("div");
+  menuItemRestaurantEl.textContent = menuItem.restaurantChain;
+  menuItemEl.appendChild(menuItemRestaurantEl);
+
+  const menuItemImageEL = document.createElement("img");
+  menuItemImageEL.src = menuItem.image;
+  menuItemEl.appendChild(menuItemImageEL);
+
+  restaurantsEl.appendChild(menuItemEl);
+}
+})();
